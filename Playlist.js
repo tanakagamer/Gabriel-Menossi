4abd257674b3820d3b79608c48754f2261e0ad46
Vandré Tanaka <tanakagamer@gmail.com>
2022-12-20 09:58:04 -0300

    Update Repository

---
+++ Playlist.js
@@ -0,0 +1,3 @@
+$(function(){
+    $('#player').html('<img src="Spotify-Logo.png"><iframe id="playlist" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7IPgINdsjBKBkrNGtHyGza?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>')
+})
---
+++ Portfolio.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('#player').click(function(){
+        $('#playlist').slideToggle();
+    })
+})
