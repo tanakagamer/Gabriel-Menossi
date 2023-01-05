c457b02a20b3cb4da137a551412d42feceff14e2
Vandré Tanaka <tanakagamerbuzanello@gmail.com>
2023-01-05 17:41:38 -0300

    Renomeando Assets

--- Portfolio.js
+++ Portfolio.js
@@ -1,6 +1,23 @@
-var videos = [$('#video1'),$('#video2')]
 $(function(){
     $('#player').click(function(){
         $('#playlist').slideToggle();
     })
 })
+
+const imgs = document.getElementById('imgs');
+const img = document.querySelector("#img img");
+
+let idx = 0
+
+function carrossel() {
+    idx++;
+
+    if(idx > img.length - 1) {
+        idx = 0;
+        
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(carrossel, 1800);
